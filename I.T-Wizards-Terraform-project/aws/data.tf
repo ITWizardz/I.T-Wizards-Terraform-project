15ed070f148aac32700e74f2a86e39d3e8214258
DGVaniX <37979538+DGVaniX@users.noreply.github.com>
2023-09-25 11:54:43 +0100

    Delete I.T-Wizards-Terraform-project directory

--- I.T-Wizards-Terraform-project/aws/data.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami" "ami_number" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*-kernel-*-x86_64"]
-  }
-}
--- I.T-Wizards-Terraform-project/aws/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = var.the_region
-  #access_key = data.vault_generic_secret.instance_key.data["access_key"]
-  #secret_key = data.vault_generic_secret.instance_key.data["secret_key"]
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
